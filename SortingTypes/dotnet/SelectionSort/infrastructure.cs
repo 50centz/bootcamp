b67c6cd58ff56bec58b22932587ecf4ce01e8b53
Федоров Евгений <zen-g@mail.ru>
2023-01-23 13:14:56 +0300

    Edited public class

--- SortingTypes/dotnet/SelectionSort/infrastructure.cs
+++ SortingTypes/dotnet/SelectionSort/infrastructure.cs
@@ -1,5 +1,5 @@
 public static class Infrastructure
-{
+{    
     public static int[] CreateArray(this int size, int min = 0, int max = 50)
     {
         return Enumerable.Range(1, size).Select(item => Random.Shared.Next(min, max) ).ToArray();
