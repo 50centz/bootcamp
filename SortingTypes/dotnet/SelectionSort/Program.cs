ac3d259a2b1ca20c7f743f7ef014110327d201d5
Федоров Евгений <zen-g@mail.ru>
2023-01-23 12:05:57 +0300

    Организация структуры проекта

--- SortingTypes/dotnet/SelectionSort/Program.cs
+++ SortingTypes/dotnet/SelectionSort/Program.cs
@@ -1,22 +1,11 @@
-﻿void SortSelection(int[] collection)
-{
-    int size = collection.Length;
+﻿using static Sorting;
+using static System.Console;
+using static Infrastructure;
 
-    for (int i = 0; i < size - 1; i++)
-    {
-        int pos = i;
-        for (int j = i + 1; j < size; j++)
-        {
-           if(collection[j] < collection[pos]) pos = j; 
-        }
-        int temp = collection[i];
-        collection[i] = collection[pos];
-        collection[pos] = temp;
-    }
-}
-
-int[] array = {7, 10, 464, 840, 1, 5, 9};
-Console.WriteLine($"[{String.Join(',' , array)}]");
+int[] array = CreateArray(10);
+Print(array);
 SortSelection(array);
-Console.WriteLine($"[{String.Join(',' , array)}]");
+Print(array);
+
+
 
