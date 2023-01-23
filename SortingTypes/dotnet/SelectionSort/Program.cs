6ac7f061b121dab96c486897375cbf56519300c3
Федоров Евгений <zen-g@mail.ru>
2023-01-23 12:50:59 +0300

    Полноценный проект сортировки и оформления проекта готов

--- SortingTypes/dotnet/SelectionSort/Program.cs
+++ SortingTypes/dotnet/SelectionSort/Program.cs
@@ -2,10 +2,20 @@
 using static System.Console;
 using static Infrastructure;
 
-int[] array = CreateArray(10);
-Print(array);
-SortSelection(array);
-Print(array);
+// int[] array = CreateArray(10, 5 , 15);
+// Print(array);
+// SortSelection(array);
+// Print(array);
+
+10.CreateArray(10 , 30)
+  .Print(" // ")
+  .SortSelection()
+  .Print();
+
+// 10.CreateArray().Print().SortSelection().Print();
 
 
 
+double res = 2.Pow(10);
+
+Console.WriteLine(res);
--- SortingTypes/dotnet/SelectionSort/infrastructure.cs
+++ SortingTypes/dotnet/SelectionSort/infrastructure.cs
@@ -1,12 +1,24 @@
 public static class Infrastructure
 {
-    public static int[] CreateArray(int size, int min = 0, int max = 50)
+    public static int[] CreateArray(this int size, int min = 0, int max = 50)
     {
         return Enumerable.Range(1, size).Select(item => Random.Shared.Next(min, max) ).ToArray();
     }
 
-    public static void Print(int[] array)
+    public static int[] Print(this int[] array, string separator = ", ")
     {
-        Console.WriteLine($"[{String.Join(',' , array)}]");
+        string output = String.Join(separator, array);
+        Console.WriteLine($"{output}]");
+        return array;
+    }
+
+    public static double Pow(this double a, double b)
+    {
+        return Math.Pow(a , b);
+    }
+
+     public static double Pow(this int a, double b)
+    {
+        return Math.Pow(a , b);
     }
 }
